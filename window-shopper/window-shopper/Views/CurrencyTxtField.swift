e14e17828996a5ab5c2ddb06ce490f777ae41eb7
ianthehuman <imavalue@gmail.comgit commit --amend --reset-authorgit config --global user.name ianthehumangit config --global user.email imavalue@gmail.com>
2017-10-28 09:33:18 -1000

    add files

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7427228744, green: 0.7427228744, blue: 0.7427228744, alpha: 0.5161601027)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
     }
-    */
 
-}
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        
+    }
+        
+        func customizeView() {
+            backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2532373716)
+            layer.cornerRadius = 5.0
+            textAlignment = .center
+            
+            clipsToBounds = true
+        
+            
+            if let p = placeholder {
+                let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+                attributedPlaceholder = place
+                textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                
+            }
+        }
+        
+    }
+
+
